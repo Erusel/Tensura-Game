fe4f84a3f2d9889bbcc7003b01dce36034e549f2
Erusel <Eruselmc@gmail.com>
2022-11-10 18:44:46 +0100

    TensuraTabCompleter fix

--- src/main/java/fr/erusel/tssdkuhc/commands/TensuraTabCompleter.java
+++ src/main/java/fr/erusel/tssdkuhc/commands/TensuraTabCompleter.java
@@ -17,12 +17,12 @@ public class TensuraTabCompleter implements TabCompleter {
 
         List<String> list = new ArrayList<>();
 
-        if (args.length == 0){
+        if (args.length == 1){
             list.add("start");
             list.add("sethost");
             list.add("giveskill");
         }
-        if (args.length == 1){
+        if (args.length == 2){
             if (args[0].equalsIgnoreCase("sethost")){
                 for (Player player : Bukkit.getOnlinePlayers()) list.add(player.getName());
             }
@@ -30,7 +30,7 @@ public class TensuraTabCompleter implements TabCompleter {
                 for (Player player : Bukkit.getOnlinePlayers()) list.add(player.getName());
             }
         }
-        if (args.length == 2){
+        if (args.length == 3){
             if (args[0].equalsIgnoreCase("giveskill")){
                 for (Skills skills : Skills.values()) list.add(skills.getSkillName());
             }
--- src/main/java/fr/erusel/tssdkuhc/objects/ActiveSkill.java
+++ src/main/java/fr/erusel/tssdkuhc/objects/ActiveSkill.java
@@ -4,6 +4,6 @@ import org.bukkit.entity.Player;
 
 public interface ActiveSkill {
 
-    public abstract void onUse(Player player);
+    void onUse(Player player);
 
 }
