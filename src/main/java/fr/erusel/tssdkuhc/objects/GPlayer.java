163ab44b3a32a793375476978198b2066975538c
Fnafgameur <chara60etdashi@gmail.com>
2022-11-12 13:19:19 +0100

    Ajouté harvestFestival Sound

--- src/main/java/fr/erusel/tssdkuhc/objects/GPlayer.java
+++ src/main/java/fr/erusel/tssdkuhc/objects/GPlayer.java
@@ -4,6 +4,8 @@ import fr.erusel.tssdkuhc.Main;
 import fr.erusel.tssdkuhc.enums.SkillTier;
 import fr.erusel.tssdkuhc.threads.HarvestFestivalRunnable;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.SoundCategory;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -83,6 +85,9 @@ public class GPlayer {
             player.sendMessage("§cVous n'avez pas les prérequis");
             return;
         }
+
+        Location location = player.getLocation();
+        player.playSound(location, "custom.sound", SoundCategory.MASTER, 1, 1);
 
         Skill skill;
         Skill sacrificeSkill = null;
